79d521f48bcbbe695c7a29251ab15d578dffd767
Cody <cghensha@gmail.com>
2013-07-27 04:22:46 +0000

    removed old shell scripts -- they were of no use.

--- shell_scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this is a comment
-mkdir trash
-mv * trash
-rm -rf trash
-mkdir trash
-echo "Deleted all files!"
-
--- shell_scripts/count.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#this program counts from 1 to 10
-for i in 1 2 3 4 5 6 7 8 9 10; do
-    echo $i
-done
-
